55ce3adb2b5186973377b5edd2dab9f5faa5a738
marc43985 <margalper3@alu.edu.gva.es>
2026-03-05 09:32:33 +0100

    Delete proyecto-citas/infra directory

--- proyecto-citas/infra/terraform-v2/locals.tf
+++
@@ -1,9 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-locals {
-  name_prefix = "${var.project_name}-${var.environment}"
-
-  # Construye DATABASE_URL en runtime usando variables y hostname de RDS.
-  # La contraseña la inyectamos vía Secrets Manager en la task definition.
-  database_url_no_password = "postgresql+psycopg2://${var.db_username}:__PASSWORD__@${aws_db_instance.postgres.address}:5432/${var.db_name}"
-}
--- proyecto-citas/infra/terraform-v2/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-image_uri = "316348991289.dkr.ecr.eu-south-2.amazonaws.com/portal-citas-api:1.0"
-
-env     = "demo"
-project = "portal-citas"
-owner   = "equipo"
-admin_token = "admin123"
